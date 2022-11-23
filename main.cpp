cb24ce7a59cbdbcfbe9b107fac856d1e5997ed51
Alexis Philippe <alexis.plp41@gmail.com>
2022-11-23 11:49:53 +0100

    test debug

--- main.cpp
+++ main.cpp
@@ -1,53 +1,33 @@
-/*#include <SFML/Graphics.hpp>
-#include <thread>
-#include <chrono>
-#include <iostream>
+#include "Game.h"
+#include "Venusaur.h"
+#include "Config.h"
 
-int main(int argc, char** argv)
+int main(void)
 {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!"); // Création fenetre
+    Game g;
 
     sf::Texture texture;
-    texture.loadFromFile("venusaur.png"); // Charge l'image dans "texture"
-    sf::IntRect rectSourceSprite(252, 251, 54, 54); // Position du perso dans l'image Sprite.jpg -> IntRect(375, 180, 25, 30)
-    sf::Sprite venusaur(texture, rectSourceSprite); // Créer le perso lui applique la texture et 
-    /*perso.setTexture(texture);
-   
-
-    sf::Clock clock; // Boucle pour animation
-    sf::Event event; // Var temporaire pour event
-
-    venusaur.setScale(4, 4); // Taille de perso
-
-    while (window.isOpen()) // Boucle pour maintenir la fenetre ouverte
+    if (!texture.loadFromFile(VENUSAUR_TEXTURE_PATH))
     {
-        while (window.pollEvent(event)) // Event fermer la fenetre
+        // error...
+    }
+
+    Venusaur venusaur = Venusaur(texture);
+
+    int count = 0;
+    while (g.isOpen())
+    {
+        g.clear();
+        g.handleEvents();
+        if (count % 20 == 0)
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            venusaur.nextAnimation();
+            count = 0;
         }
-
-        if (clock.getElapsedTime().asSeconds() > 1.0f) { // Boucle d'animations
-            if (rectSourceSprite.top > 380){
-                rectSourceSprite.top = 251;  // InteRect de base, perso original
-            }
-            else if(rectSourceSprite.top == 315) { // Si il a fait un pas alors en fait un deuxieme
-              rectSourceSprite.top += 65; 
-            }
-            else {
-                rectSourceSprite.top += 66; // Change le IntRect pour que perso change de "skin" et face un pas
-            }
-
-            venusaur.setTextureRect(rectSourceSprite); // Donne la nouvelle position a perso
-            clock.restart(); // Restart clock...?
-        }
-
-        window.clear();
-
-        window.draw(venusaur);
-
-        window.display();
+        count++;
+        g.drawEntity(venusaur);
+        g.display();
     }
 
     return 0;
-}*/
+}
