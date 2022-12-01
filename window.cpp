6b3ccab9a382eb00013993b4ded6287acc269872
juju <juju.doucet@hotmail.com>
2022-12-01 22:08:11 +0100

    add element de dialogue

--- window.cpp
+++ window.cpp
@@ -8,6 +8,10 @@ bool right = false;
 bool down = false;
 bool menu = true;
 bool jeu = false;
+bool dialogue = false;
+bool combat = false;
+bool deplacement = true;
+bool next = false;
 
 manage_window::manage_window() : window(sf::VideoMode(960, 720), "Pokemon")
 {
@@ -67,6 +71,34 @@ void manage_window::open_close_window()
     sf::View view;
     view.setViewport(sf::FloatRect(0, 0, 1.f, 1.f));
     const float speed = 1500.f;
+    int dealtxt = 1;
+    bool next = false;
+    sf::Text Deal;
+    sf::Font police;
+    police.loadFromFile("PKM_font.ttf");
+    Deal.setFont(police);
+    Deal.setString("Donne la cam");
+    Deal.setCharacterSize(50);
+    Deal.setFillColor(sf::Color::White);
+    Deal.setPosition(2320, 2950);
+    sf::Text Deal2;
+    Deal2.setFont(police);
+    Deal2.setString("Non");
+    Deal2.setCharacterSize(50);
+    Deal2.setFillColor(sf::Color::White);
+    Deal2.setPosition(2320, 2950);
+    sf::Text Deal3;
+    Deal3.setFont(police);
+    Deal3.setString("ok");
+    Deal3.setCharacterSize(50);
+    Deal3.setFillColor(sf::Color::White);
+    Deal3.setPosition(2320, 2950);
+    sf::Texture texture_box;
+    texture_box.loadFromFile("zone_texte.png");
+    sf::IntRect rectBox(0, 2, 235, 44);
+    sf::Sprite box(texture_box, rectBox);
+    box.setScale(4, 4);
+    box.setPosition(2280, 2900);
     Musique m;
     m.song_menu_principal();
     while (isOpen())
@@ -160,6 +192,16 @@ void manage_window::open_close_window()
             else if (perso.getPosition().y > 3000) {
                 perso.setPosition(posX, 3000);
             }
+            if (posX < 2800 && posX > 2700 && posY < 2770 && posY > 2500) {
+                deplacement = false;
+                perso.setPosition(2742, 2600);
+                rectSourceSprite.left = 11;
+                rectSourceSprite.top = 4;
+                perso.setTextureRect(rectSourceSprite);
+                next = true;
+
+
+            }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
                 right = true;
                 up = false;
@@ -211,11 +253,18 @@ void manage_window::open_close_window()
                 }
                 while (this->window.pollEvent(event)) {
                     if (event.type == sf::Event::Closed) {
-                        window.close();
+                        this->window.close();
                     }
                     if (event.type == sf::Event::KeyPressed) {
                         if (event.key.code == sf::Keyboard::Escape) {
                             menu_create();
+                        }
+                    }
+                    if (next) {
+                        if (event.type == sf::Event::KeyPressed) {
+                            jeu = false;
+                            dialogue = true;
+
                         }
                     }
                 }
@@ -228,5 +277,60 @@ void manage_window::open_close_window()
             manage_event();
             display_window();
         }
+        if (dialogue) {
+            this->window.clear();
+            this->window.setView(view);
+            this->window.draw(background);
+            this->window.draw(dealer.get_sprite());
+            this->window.draw(perso);
+
+            /*rectDealo.left = 20;
+            rectDealo.top = 0;
+            rectDealo.width = 120;
+            rectDealo.height = 80;*/
+            //view.setCenter(2742, 2600);
+            //dealo.setTextureRect(rectDealo);
+            //dealo.setScale(11, 11);
+            //dealo.setPosition(2100, 2100);
+
+            if (dealtxt == 1) {
+                this->window.draw(box);
+                this->window.draw(Deal);
+            }
+            if (dealtxt == 2) {
+                this->window.draw(box);
+                this->window.draw(Deal2);
+            }
+            if (dealtxt == 3) {
+                this->window.draw(box);
+                this->window.draw(Deal3);
+            }
+            if (this->window.pollEvent(event)) {
+                if (event.type == sf::Event::KeyPressed) {
+                    if (dealtxt == 1) {
+                        dealtxt++;
+                    }
+                    else if (dealtxt == 2) {
+                        dealtxt++;
+                    }
+                    else if (dealtxt == 3) {
+                        jeu = false;
+                        combat = true;
+                    }
+
+
+                }
+
+                if (event.type == sf::Event::Closed) {
+                    this->window.close();
+                }
+
+            }
+
+
+            this->window.display();
+
+        }
+
     }
 }
